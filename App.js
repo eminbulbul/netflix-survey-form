4f7cb1d303c76d8bad3b596e61ccab713f4e76d6
m_emin <mehmeteminbul@gmail.com>
2022-02-08 23:41:27 +0300

    session ended

--- App.js
+++ App.js
@@ -39,3 +39,55 @@
 // let lastname1=document.getElementById("lastname2")
 // lastname1.appendChild(input1)
 // input1.innerHTML='<input type="email" name="email" id="email" placeholder="Enter your email">süheyl</input>'
+
+
+// const firstname=document.getElementById("firstname")
+// console.log(firstname);
+// const lastname=document.getElementsByClassName("input-box")[1]
+// const lastname1=document.getElementById("lastname")
+// console.log(lastname1);
+// console.log(lastname);
+// const age =document.querySelectorAll(".input-box")[2]
+// console.log(age);
+// console.log(age.getAttribute("Class"));
+// let alarm=function(){
+//     alert("Çerez politikasını onaylıyor musunuz")
+// }
+// firstname.setAttribute("onclick","alarm()")
+// //lastname1.setAttribute("type","radio")
+// let üstdiv=document.getElementById("survey-form-netflix")
+// let fullname=document.createElement("input")
+// üstdiv.appendChild(fullname)
+// fullname.innerHTML+='<input type="text" name="fullname" id="fullname" placeholder="Enter your fullname"></input>';
+// let lastName=document.querySelector("input[name=last_name]");
+// let charSpan=document.getElementById("charCount");
+// lastName.addEventListener("keyup",()=>{
+//     charSpan.innerText=lastName.value.length +"/40"; });
+//  const firstnameıd=document.getElementById("firstname");
+// console.log(firstnameıd);
+// const lastnameclass=document.querySelector(".input-box")
+// console.log(lastnameclass);
+// const firstname12=document.getElementsByClassName("input-box")
+// console.log(firstname12);
+// let alarm=function(){
+//     alert("çerez politikamızı onaylıyormusunuz")
+// }
+// firstnameıd.setAttribute("onclick","alarm()");
+// firstnameıd.setAttribute("type","radio")
+// let input1=document.createElement("span");
+// let lastname1=document.getElementById("lastname2")
+// lastname1.appendChild(input1)
+// input1.innerHTML='<input type="email" name="email" id="email" placeholder="Enter your email">süheyl</input>'
+
+// console.log(new Date().getFullYear());
+// let span1=document.getElementById("charCount");
+// let lastname1=document.getElementById("lastname");
+// lastname1.addEventListener("keyup",()=>{
+//     if (lastname1.value.length==0){
+//         span1.style.display="none"
+//     }
+//     else {
+//         span1.innerText=lastname1.value.length +"/40";
+//         span1.style.display="inline";
+//     }
+// }
